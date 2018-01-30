aa9fd41c7737295c21c3107e21dba4a4afe427bd
abas <akhmadbasir5@gmail.com>
2018-01-31 00:49:06 +0700

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('level',['admin','student','investor']);
             $table->rememberToken();
             $table->timestamps();
         });
