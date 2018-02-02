318a658575889daef9a9b31acc3ab6a3bfb02a2d
abas <akhmadbasir5@gmail.com>
2018-02-02 23:07:34 +0700

    define table

--- app/Campaign.php
+++ app/Campaign.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Campaign extends Model
 {
-    //
+    protected $table = 'campaigns';
+    protected $fillable = [
+        'cover',
+        'judul',
+        'diskripsi',
+        'link_youtube',
+        'kebutuhan_dana',
+        'pengembalian_dana',
+        'bagi_hasil',
+        't_judul',
+        't_deskripsi',
+        'category_campaign_id',
+        'user_id',
+        'user_id'
+    ];
+
+    protected $hidden = [
+        'bagi_hasil'
+    ];
+
+    protected $guard = [
+        'bagi_hasil'
+    ];
+
+    
 }
