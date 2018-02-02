2eb00ff575765e07b3b351e64380e6eeac982fcc
abas <akhmadbasir5@gmail.com>
2018-02-02 11:19:32 +0700

    config dashboard index done

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,17 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User,App\Investor,App\Student;
 
 class DashboardController extends Controller
 {
+    public $user;
     public function __construct()
     {
         $this->middleware('auth');
+        $this->f_user = new User;
     }
 
     public function dashboard()
     {
-        return view('dashboard.dashboard');
+        $id = Auth::user()->id;
+        $user = User::find($id)->first();
+        if($this->f_user::isStudent($user->level)){
+            $data = Student::where('user_id','=',$id)->get()->first();
+        }
+        
+        // $info = json_encode([$user,$data]);
+        $isverif = false;
+        if($user->isVerify($user->id)){
+            $isverif = true;
+        }
+        return view('dashboard.dashboard',compact('user','data','isverif'));
     }
 
     public function dashboardBlog()
