1040456aac0a5c618190be6c0ad49d7a268353ca
abas <akhmadbasir5@gmail.com>
2018-02-03 10:43:01 +0700

    reconfig blog show for user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,22 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User,App\Investor,App\Student;
+use App\Blog,App\Campaign;
 
 class DashboardController extends Controller
 {
     public $user;
     public function __construct()
     {
-        $this->middleware('auth');
-        $this->f_user = new User;
+        // $this->middleware('auth');
+        $this->a_user = new User;
     }
 
     public function dashboard()
     {
         $user = Auth::user();
-        if($this->f_user::isStudent($user->level)){
+        if($this->a_user::isStudent($user->level)){
             $data = Student::where('user_id','=',$user->id)->get()->first();
-        }else if($this->f_user::isInvestor($user->level)){
+        }else if($this->a_user::isInvestor($user->level)){
             $data = Investor::where('user_id','=',$user->id)->get()->first();
         }
 
@@ -37,7 +38,15 @@ class DashboardController extends Controller
 
     public function dashboardBlog()
     {
-        return view('dashboard.dashboard-blog');
+        // $user = Auth::user();
+        // $blogs = Blog::where('user_id',$user->id)->get();
+        $blogs = Blog::where('user_id',18)->get();
+        if($blogs==null){
+            return "no more blogs here!";
+        }
+        return $blogs;
+
+        // return view('dashboard.dashboard-blog');
     }
 
 
