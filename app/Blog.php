8c9a007266c2c3aaddce7fbe5ae6553bbc843cc3
abas <akhmadbasir5@gmail.com>
2018-02-03 10:41:33 +0700

    protec user_id from blog

--- app/Blog.php
+++ app/Blog.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     protected $table = 'blogs';
+    protected $guard = ['user_id'];
     protected $fillable = [
         'judul','cover_file','deskripsi',
         'category_id','user_id'
     ];
 
-    // protected $guard = ['user_id'];
     // protected $hidden = ['user_id'];
     
     
